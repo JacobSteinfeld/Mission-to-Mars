671cd26b3dfe0df982c66d8486181b5506a8224e
Jacob Steinfeld <JSTEINY@Jacobs-Air.attlocal.net>
2021-10-31 20:37:56 -0700

    Final Push

--- scraping.py
+++ scraping.py
@@ -1,4 +1,5 @@
 # Import Splinter and BeautifulSoup
+from os import WCOREDUMP
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,14 +12,16 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
-
+    hemisphere_image_urls = hemi_data(browser)
+   
     # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_image_urls
     }
 
     # Stop webdriver and return data
@@ -96,6 +99,39 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
+
+def hemi_data(browser):
+   # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+   # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+    a_indices = [0,2,4,6]
+    mars_pics = img_soup.find_all('a', class_='itemLink product-item')
+    #print(mars_pics)
+    for a in a_indices:
+        mars_pic_href = mars_pics[a].get('href')
+        mars_pic_url = url + mars_pic_href
+        browser.visit(mars_pic_url)
+        html_pic = browser.html
+        get_pic = soup(html_pic, 'html.parser')
+        mars_get_image = get_pic.find_all('a', target = "_blank")[2].get('href')
+        final_url = url + mars_get_image
+        page_dict = {}
+        page_dict["img_url"]= final_url
+        get_name = get_pic.find_all('h2', class_ = "title")
+        for name in get_name:
+            word = name.text
+            print(word)
+            page_dict["title"] = word
+        hemisphere_image_urls.append(page_dict)
+
+    return hemisphere_image_urls
+
 
 
 if __name__ == "__main__":
